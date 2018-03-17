fdfd90e6a8c06efc1e031df78df03f7084023025
KNhan <31634223+KNhan@users.noreply.github.com>
2018-03-17 17:08:11 +0100

    Add files via upload

--- hackLux2.py
+++ hackLux2.py
@@ -6,6 +6,7 @@ import os
 import requests
 import logging
 import pdb
+from requests.exceptions import ConnectionError
 
 #oauth access to the new dataset
 url = "https://iotmmsa2618100a.hana.ondemand.com/com.sap.iotservices.mms/v1/api/http/data/fb6e9682-d60b-492a-8a09-aadbea847117"
@@ -69,64 +70,73 @@ def get_present_gps():
                                                 pass
 
 while True:
-        time.sleep(5)
-        #get request
-        response = requests.request("GET", url, headers=headers).json()
-        print "first get"
-        print response
-        if response != []:        
-                data = response[0]["messages"]
-
-                buttonStatus = data[0]["trigger"]
-                tripId = data[0]["tripid"]
-
-
-        #post request
-        while (buttonStatus == "start"):                
-                print "start collecting data"
-
-                response = requests.request("GET", url, headers=headers).json()
-                print "second get and changing start into stop ?"
-                print response
-                
-                if response != []:
+		#check the internet connection
+        try:
+                 print "asking for a network"
+                 time.sleep(5)
+                 #get request
+                 response = requests.request("GET", url, headers=headers).json()
+                 print "first get"
+                 print "stop collecting data"
+                 print response
+                 if response != []:        
                         data = response[0]["messages"]
+
                         buttonStatus = data[0]["trigger"]
-                        tripId = data[0]["tripid"] + 1
+                        tripId = data[0]["tripid"]+1
 
-                t.flushInput()  
-                position = get_present_gps()    
-                retstr = t.read(10)
-        
-                hexShow(retstr)
-                #print hexShow(retstr)
-        
-                if len(retstr)==10:
-                        if(retstr[0]==b"\xaa" and retstr[1]==b'\xc0'):
-                                checksum=0
-                                #pdb.setTrace()
-                                for i in range(6):
-                                    checksum=checksum+ord(retstr[2+i])
-                                if checksum%256 == ord(retstr[8]):
-                                    pm25_aux=ord(retstr[2])+ord(retstr[3])*256
-                                    pm25=str(pm25_aux/10.0)
-                                    pm10_aux=ord(retstr[4])+ord(retstr[5])*256
-                                    pm10=str(pm10_aux/10.0)
-                                    #print pm25 , pm10
 
-                #post request
-                headers = {
-                'Content-Type':"application/json",
-                'Authorization': "Bearer f55197ba5cfd788f4454ab8c95ca64c",
-                'Cache-Control': "no-cache",
-                }
-        
-                payload = "{\"mode\":\"sync\",\"messageType\":\"d225176ae8fb5d12880e\",\"messages\":[{\"timestamp\":"+ str(int(ts)) +", \"tripid\":"+str(tripId)+",\"pm2\":\""+pm25+"\", \"pm10\":\""+pm10+"\", \"lat\": \""+ str(position[0]) +"\" , \"long\":\""+str(position[1])+"\"}]}"
-                logging.info(payload)
-                response_post = requests.request("POST", url, data=payload, headers=headers)
+                 #post request
+                 while (buttonStatus == "start"):
+                        
+                        print "start collecting data"
 
-                print(response_post.text)
-
-        print "stop collecting data"
+                        response = requests.request("GET", url, headers=headers).json()
+                        print "get next data"
+                        #print response
                 
+                        if response != []:
+                                data = response[0]["messages"]
+                                buttonStatus = data[0]["trigger"]
+                                tripId = data[0]["tripid"]
+
+                        t.flushInput()  
+                        position = get_present_gps()    
+                        retstr = t.read(10)
+        
+                        hexShow(retstr)
+                        #print hexShow(retstr)
+        
+                        if len(retstr)==10:
+                                if(retstr[0]==b"\xaa" and retstr[1]==b'\xc0'):
+                                        checksum=0
+                                        #pdb.setTrace()
+                                        for i in range(6):
+                                            checksum=checksum+ord(retstr[2+i])
+                                        if checksum%256 == ord(retstr[8]):
+                                            pm25_aux=ord(retstr[2])+ord(retstr[3])*256
+                                            pm25=str(pm25_aux/10.0)
+                                            pm10_aux=ord(retstr[4])+ord(retstr[5])*256
+                                            pm10=str(pm10_aux/10.0)
+                                            print pm25 , pm10
+
+                        #post request
+                        headers = {
+                        'Content-Type':"application/json",
+                        'Authorization': "Bearer f55197ba5cfd788f4454ab8c95ca64c",
+                        'Cache-Control': "no-cache",
+                        }
+                
+                        payload = "{\"mode\":\"sync\",\"messageType\":\"d225176ae8fb5d12880e\",\"messages\":[{\"timestamp\":"+ str(int(ts)) +", \"tripid\":"+str(tripId)+",\"pm2\":\""+pm25+"\", \"pm10\":\""+pm10+"\", \"lat\": \""+ str(position[0]) +"\" , \"long\":\""+str(position[1])+"\"}]}"
+                        logging.info(payload)
+                        response_post = requests.request("POST", url, data=payload, headers=headers)
+
+                        print(response_post.text)
+
+                 print "stop collecting data"
+        except ConnectionError as e:
+                print e
+                print "no connection "
+                
+                        
 
